658f36edbdaeb8b81b729f962bd9f9edcf85ebc5
Parimala <Parimala@Parimala>
2022-11-25 22:10:14 -0800

    updated

--- src/test/java/unit/Exp11/AppTest.java
+++ src/test/java/unit/Exp11/AppTest.java
@@ -2,21 +2,11 @@ package unit.Exp11;
 
 import static org.junit.Assert.assertTrue;
 
-import org.junit.Test;
 
-/**
- * Unit test for simple App.
- */
 public class AppTest 
 {
-    /**
-     * Rigorous Test :-)
-     */
+ 
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
-    }
     public void test()
 	{
 		App p=new App();
